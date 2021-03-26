8ead3cd7dec24f31c85feca74490afa9380f29b3
aolteanu10 <alexolteanu10@icloud.com>
2021-03-26 02:27:16 +0200

    V2.0

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggleBtn = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
+
 toggleBtn.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
