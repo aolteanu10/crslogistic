8a1caaf847709bcb09536427104528e38ae907fe
Alexandru Olteanu <olteanua@coventry.ac.uk>
2021-07-08 15:43:07 +0300

    Finished

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const toggleBtn = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const inputs = document.querySelectorAll(".input");
 
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value === "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+})
 
 toggleBtn.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
